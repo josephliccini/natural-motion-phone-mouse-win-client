3da54f4fe50a32827954bea6ff00559b31cb8774
Joseph Liccini <joseph.liccini@ufl.edu>
2016-01-28 15:08:03 -0500

    Short Message logic on Closing in on Desired Point on Client, not Android Server

--- NaturalMotionPhoneMouseWindowsClient/TestDataDisplacementTranslator.cs
+++ NaturalMotionPhoneMouseWindowsClient/TestDataDisplacementTranslator.cs
@@ -9,15 +9,30 @@ namespace NaturalMotionPhoneMouseWindowsClient
 {
     class TestDataDisplacementTranslator : DisplacementTranslator
     {
+        private double shortMessageCount = 0.0;
+
         public override MouseMotionDelta TranslateData(JObject json)
         {
             var x = json.GetValue("displacementX").Value<double>();
             var y = json.GetValue("displacementY").Value<double>();
 
+            double combinedDisplacement = Math.Abs(x) + Math.Abs(y);
+
+            if (combinedDisplacement > 3.0)
+            {
+                shortMessageCount = 0.0;
+            }
+            else
+            {
+                ++shortMessageCount;
+            }
+
+            double multiplicativeFactor = Math.Max(1.5 - (shortMessageCount / 2.0), 0);
+
             return new MouseMotionDelta
             {
-                DisplacementX = Math.Floor(x) * 1.3,
-                DisplacementY = Math.Floor(y) * 1.3
+                DisplacementX = Math.Floor(x) * multiplicativeFactor,
+                DisplacementY = Math.Floor(y) * multiplicativeFactor 
             };
         }
     }
