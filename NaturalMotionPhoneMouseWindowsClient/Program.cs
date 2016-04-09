9a97fe4eef9d1c561ab0c5596499d7c019ce71c3
Joseph Liccini <joseph.liccini@ufl.edu>
2016-04-09 14:52:52 -0400

    Only one instance of the application can run on Windows at a time

--- NaturalMotionPhoneMouseWindowsClient/Program.cs
+++ NaturalMotionPhoneMouseWindowsClient/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -8,15 +9,21 @@ namespace NaturalMotionPhoneMouseWindowsClient
 {
     static class Program
     {
-        /// <summary>
-        /// The main entry point for the application.
-        /// </summary>
+        private static Mutex m_Mutex;
+
         [STAThread]
         static void Main()
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+
+            bool createdNew;
+            m_Mutex = new Mutex(true, "MyApplicationMutex", out createdNew);
+            if (createdNew)
+                Application.Run(new Form1());
+            else
+                MessageBox.Show("The application is already running.", Application.ProductName,
+                  MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
         }
     }
 }
